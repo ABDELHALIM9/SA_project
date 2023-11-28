995f63297c2099be8659de7a1385038b9d039fb9
Mohey Eldeen Maher <54249650+mohey-e-maher@users.noreply.github.com>
2023-11-28 19:35:28 +0200

    add_internal_profile &edits

--- js/main.js
+++ js/main.js
@@ -96,3 +96,16 @@
     
 })(jQuery);
 
+// chart scripts 
+$(document).ready(function() {
+	$('.project-profitability').addClass('active');
+});
+
+
+//replay button, just removing and reattaching the active class 
+$('.js-replay').click(function() {
+	$('.project-profitability').removeClass('active');
+	setTimeout(function() {
+		$(".project-profitability").addClass("active");
+	}, 500);
+});
